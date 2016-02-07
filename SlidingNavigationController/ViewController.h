24d8d936eadcab16b3435b6b4f443ef0c0dc3a69
Chibaibuki <budingmail@vip.qq.com>
2016-02-07 23:04:35 +0800

    Initial Commit

---
+++ SlidingNavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlidingNavigationController
+//
+//  Created by 高向孚 on 16/2/7.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlidingNavigationController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlidingNavigationController
+//
+//  Created by 高向孚 on 16/2/7.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
